cb101fa4de4d14bd06ae974cea59ee1ecacc1777
Joseph Ayako <jayako@udel.edu>
2024-09-06 22:23:33 -0400

    Completed task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,10 +8,38 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Joseph Ayako
             </header>
+            <h1> Amazing Header</h1>
             <p>
                 Hello World Edit <code>src/App.tsx</code> and save. This page
                 will automatically reload.
             </p>
+
+            <img
+                src="https://preview.redd.it/the-pope-drip-v0-de6cf3omoqpa1.jpg?width=640&crop=smart&auto=webp&s=4efaaaee0265a5923e7804af69e62fdac8461b3c"
+                alt="Drippy Pope"
+            />
+
+            <ul>
+                <li>Dogs</li>
+                <li>Cats</li>
+                <li>Parrots</li>
+            </ul>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <div
+                style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "#FF0000",
+                }}
+            ></div>
         </div>
     );
 }
